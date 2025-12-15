6636721eca46fbb857c46431bd9dd38f43bc5161
vs-123 <19247154+vs-123@users.noreply.github.com>
2025-12-15 02:10:36 +0000

    fix typos

--- royal-hemlock-theme.el
+++ royal-hemlock-theme.el
@@ -16,7 +16,7 @@
 
 ;; A light-theme with the core colour being Royal Blue for Emacs 24+.
 ;; The theme is accompanied by a warm white background for a soothing
-;; experience.
+;; experience
 
 ;;; Code:
 
